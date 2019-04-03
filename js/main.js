2bab5173f7ae2d60ac5aa51a27b7fe584c509b91
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-04-03 15:35:52 -0300

    places image background

--- js/main.js
+++ js/main.js
@@ -11,6 +11,22 @@ let w = 1080,
 let n_mods = 24;
 let mod = w / n_mods;
 
+let backgrounds = [
+  { src: "../assets/backgrounds/01.jpg", img: null },
+  { src: "../assets/backgrounds/02.jpg", img: null },
+  { src: "../assets/backgrounds/03.jpg", img: null },
+  { src: "../assets/backgrounds/04.jpg", img: null },
+  { src: "../assets/backgrounds/05.jpg", img: null },
+  { src: "../assets/backgrounds/06.jpg", img: null },
+  { src: "../assets/backgrounds/07.jpg", img: null },
+  { src: "../assets/backgrounds/08.jpg", img: null },
+  { src: "../assets/backgrounds/09.jpg", img: null },
+  { src: "../assets/backgrounds/10.jpg", img: null },
+  { src: "../assets/backgrounds/11.jpg", img: null },
+  { src: "../assets/backgrounds/12.jpg", img: null },
+  { src: "../assets/backgrounds/13.jpg", img: null }
+];
+
 function preload() {
   myFont = loadFont("../assets/antropos.otf");
   logo = loadImage("../assets/logo.svg");
@@ -24,23 +40,21 @@ function setup() {
 function draw() {
   background(0);
 
-  // draw_logo(1, 1);
-  // draw_logo(n_mods - mods_logo - 1, 1);
-  // draw_logo(1, n_mods - mods_logo - 1);
-  draw_logo(n_mods - mods_logo - 1, n_mods - mods_logo - 1);
+  if (draw_background(0)) {
+    // draw_logo(1, 1);
+    // draw_logo(n_mods - mods_logo - 1, 1);
+    // draw_logo(1, n_mods - mods_logo - 1);
+    draw_logo(n_mods - mods_logo - 1, n_mods - mods_logo - 1);
 
-  draw_text(
-    "Blandit mi id neque dignissim fusce lorem morbi ut vestibulum netus vitae lectus suscipit ullamcorper enim magnis vestibulum est bibendum vestibulum nec suscipit ac. Parturient lectus pulvinar sagittis luctus a a hendrerit vestibulum leo parturient a arcu vulputate euismod dolor elit parturient vitae in eleifend condimentum ullamcorper pretium eu cubilia posuerelaoreet facilisi. Scelerisque hac nec mauris mattis nascetur est cursus eros ac class suspendisse donec nibh parturient mi vestibulum curabitur mi. Suspendisse vitae velit vestibulum habitant varius inceptos leo mi a ullamcorper erat pulvinar a quis luctus nisi sed. Facilisi elementum vestibulum parturient id ullamcorper potenti habitasse metus"
-  );
+    draw_text(
+      "Blandit mi id neque dignissim fusce lorem morbi ut vestibulum netus vitae lectus suscipit ullamcorper enim magnis vestibulum est bibendum vestibulum nec suscipit ac. Parturient lectus pulvinar sagittis luctus a a hendrerit vestibulum leo parturient a arcu vulputate euismod dolor elit parturient vitae in eleifend condimentum ullamcorper pretium eu cubilia posuerelaoreet facilisi. Scelerisque hac nec mauris mattis nascetur est cursus eros ac class suspendisse donec nibh parturient mi vestibulum curabitur mi. Suspendisse vitae velit vestibulum habitant varius inceptos leo mi a ullamcorper erat pulvinar a quis luctus nisi sed. Facilisi elementum vestibulum parturient id ullamcorper potenti habitasse metus"
+    );
+  } else {
+    draw_text("carregando \n imagem....");
+  }
 
   if (debug) {
-    stroke(255, 0, 0, 128);
-    for (let x = 1; x <= n_mods; x++) {
-      line(x * mod, 0, x * mod, height);
-    }
-    for (let y = 1; y <= n_mods; y++) {
-      line(0, y * mod, width, y * mod);
-    }
+    draw_grid();
   }
 
   noLoop();
@@ -60,6 +74,16 @@ function grid(x, y) {
     return null;
   }
   return createVector(_mod(x), _mod(y));
+}
+
+function draw_grid() {
+  stroke(255, 0, 0, 128);
+  for (let x = 1; x <= n_mods; x++) {
+    line(x * mod, 0, x * mod, height);
+  }
+  for (let y = 1; y <= n_mods; y++) {
+    line(0, y * mod, width, y * mod);
+  }
 }
 
 /// drawing functions
@@ -114,4 +138,27 @@ function draw_text(t) {
   textSize(ts);
   fill(255);
   text(t, tx, ty, tw, th);
+}
+
+function place_background(n) {
+  image(backgrounds[n].img, 0, 0);
+  redraw();
+}
+
+function draw_background(n = 0) {
+  if (n > backgrounds.length) {
+    return null;
+  }
+  if (backgrounds[n].img == null) {
+    // will wait until image is load to continue drawing
+    loadImage(backgrounds[n].src, img => {
+      loop();
+      backgrounds[n].img = img;
+      place_background(n);
+    });
+    return false;
+  } else {
+    image(backgrounds[n].img, 0, 0);
+    return true;
+  }
 }
